9c9d0760151d8bfa52e7dd94b2e358875d59c9af
Shane Mc Cormack <shanemcc@gmail.com>
2017-05-14 22:37:41 +0100

    Updates to bind configs.

--- deploy_local_slave.sh
+++ deploy_local_slave.sh
@@ -1,6 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 ################################################################################
-# This script will handle the automatic setup of a slave nameserver	           #
+# This script will handle the automatic setup of a slave nameserver	locally    #
+# rather than via docker.                                                      #
+#                                                                              #
+# This requires that bind9 from apt is at least 9.11 or later.     	           #
+################################################################################
+# apt-get update && apt-get -y install git && git clone https://github.com/shanemcc/mydnshost-bind && cd mydnshost-bind && sh deploy_local_slave.sh
 ################################################################################
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -23,18 +28,32 @@ if [ "${HASBIND}" = "" ]; then
 fi;
 
 echo "Removing old bind config...";
-rm -Rfv "/etc/bind/*";
+rm -Rfv "/etc/bind/"*;
 
 echo "Adding new bind config...";
-cp -Rfv "${DIR}/bind/*" "/etc/bind/";
+TEMPDIR=`mktemp -d`
+git clone https://github.com/nguoianphu/docker-dns ${TEMPDIR}
+
+cp -Rfv "${TEMPDIR}/bind/"* "/etc/bind/";
+cp -Rfv "${DIR}/bind/"* "/etc/bind/";
+rm -Rf "${TEMPDIR}"
+
+mkdir -p /etc/bind/dynamic
+mkdir -p /etc/bind/data
+touch /etc/bind/data/named.run
 
 rm -Rfv "/etc/bind/named.conf";
 ln -sf "/etc/bind/named.slave.conf" "/etc/bind/named.conf";
 
 echo "Creating cat-zones directory...";
 mkdir /etc/bind/cat-zones/
-chown bind:bind /etc/bind/cat-zones/
+
+# echo "Removing Catalog-Zones configuration...";
+# sed -i -e '1h;2,$H;$!d;g' -e 's/catalog-zones {[^}]*};[^}]*};[^}]*};//g' /etc/bind/named.slave.conf
+
+echo "Fixing ownership";
+chown -Rf bind:bind /etc/bind
 
 echo "Restarting bind."
-RNDC=`which rndc`
-${RNDC} reload
+service bind9 stop
+service bind9 start
