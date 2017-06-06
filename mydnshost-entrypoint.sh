5641996b3b1f5ea2aea2a7a71458aee79eaa6fff
Shane Mc Cormack <shanemcc@gmail.com>
2017-06-06 01:58:47 +0100

    Support allowed-transfer in catalog.

--- mydnshost-entrypoint.sh
+++ mydnshost-entrypoint.sh
@@ -11,13 +11,23 @@ if [ "$1" == "" ]; then
 
 		# Rebuild _default.nzf
 		ZONEFILE="/etc/bind/_default.nzf"
+		CATALOGFILE="/bind/catalog.db"
 
 		echo "# New zone file for view: _default" > "${ZONEFILE}"
 		echo "# This file contains configuration for zones added by" >> "${ZONEFILE}"
 		echo "# the 'rndc addzone' command. DO NOT EDIT BY HAND." >> "${ZONEFILE}"
 
-		cat /bind/catalog.db | egrep "IN[[:space:]]+PTR" | awk -F" PTR " '{print $2}' | sed 's/.$//' | while read ZONE; do
-			echo 'zone "'${ZONE}'" { type master; file "/bind/zones/'${ZONE}'.db"; };' >> ${ZONEFILE}
+		cat "${CATALOGFILE}" | egrep "IN[[:space:]]+PTR" | while read LINE; do
+			ZONE=$(echo "${LINE}" | awk -F" PTR[[:space:]]+" '{print $2}' | sed 's/.$//');
+			HASH=$(echo "${LINE}" | awk -F".zones[[:space:]]+" '{print $1}');
+			ALLOWED_TRANSFER=$(cat "${CATALOGFILE}" | egrep "allow-transfer.${HASH}.zones[[:space:]]+" | awk -F" APL " '{print $2}');
+
+			if [ "${ALLOWED_TRANSFER}" = "" ]; then
+				echo 'zone "'${ZONE}'" { type master; file "/bind/zones/'${ZONE}'.db"; };' >> ${ZONEFILE}
+			else
+				ALLOWED_TRANSFER=$(echo "${ALLOWED_TRANSFER}" | sed -re 's#[12]:([^/]+)/(32|128)#\1;#g');
+				echo 'zone "'${ZONE}'" { type master; file "/bind/zones/'${ZONE}'.db"; allow-transfer { '${ALLOWED_TRANSFER}' }; };' >> ${ZONEFILE}
+			fi;
 		done;
 
 		exec named -c /etc/bind/named.master.conf -g
